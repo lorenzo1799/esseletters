61ba0fb943a2e12810196a5d6f08fcb28a872c56
Lorenzo1799 <lorenzogwada@gmail.com>
2023-10-20 21:54:10 +0200

    scroll images

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Animazione per le immagini a sinistra
+gsap.to(".left", {
+    x: 0,
+    opacity: 1,
+    stagger: 0.5, // Ritardo tra ogni animazione
+    scrollTrigger: {
+        trigger: ".container",
+        start: "top 75%", // La posizione dello scroll quando inizia l'animazione
+        end: "bottom 25%",
+        markers: false, // Mostra i marker per il debug
+        scrub: true
+    }
+});
+
+// Animazione per le immagini a destra
+gsap.to(".right", {
+    x: 0,
+    opacity: 1,
+    stagger: 0.5, 
+    scrollTrigger: {
+        trigger: ".container",
+        start: "top 75%",
+        end: "bottom 25%",
+        markers: false,
+        scrub: true
+    }
+});
+
 var active = 3;
 
 var mncircles = document.querySelectorAll(".mn-circle");
@@ -12,13 +42,15 @@ var stripes = document.querySelectorAll(".head-h4");
 
 var herotonav = document.querySelector(".hero-to-nav");
 
-var changemodecircle=document.querySelector(".change-dark-light");
+var changemodecircle = document.querySelector(".change-dark-light");
 
 var esseElement = document.querySelector(".imgcont");
 
 var copyright = document.querySelector(".copyright");
 
-var rainbowHover = document.querySelector(".rainbow-hover")
+var rainbowHover = document.querySelector(".rainbow-hover");
+
+
 
 
 const lerp = (x, y, a) => x * (1 - a) + y * a;
@@ -223,24 +255,57 @@ herotonav.addEventListener("mouseleave", function () {
 
 
 
+
+
 //                                  DARK       MODE ON
 
 
-changemodecircle.addEventListener("click",function(){
-    document.querySelector(".change-mode-circle").classList.toggle("change-mode-circle-class");
-    document.querySelector(".change-mode-circle i").classList.toggle("fa-moon");
-    document.querySelector(".changeinputdarklight").classList.toggle("input-backgroundchange");
-    document.querySelector(".hero-section #madeby").classList.toggle("madebydark");
-    document.querySelector(".hero-section .block").classList.toggle("block-dark");
-    document.querySelector(".name").classList.toggle("name-dark");
-    //document.querySelector(".hero-to-nav a i").classList.toggle("ii-color-white")
-    document.querySelector(".hero-section").classList.toggle("ondark-hero");
-    document.querySelector(".nav").classList.toggle("main-dark");
-    document.querySelector(".nav #madeby").classList.toggle("madebydark");
-    document.querySelector(".nav .block").classList.toggle("block-dark-nav");
-})
+document.addEventListener("DOMContentLoaded", function() {
+    changemodecircle.addEventListener("click",function(){
+        document.querySelector(".change-mode-circle").classList.toggle("change-mode-circle-class");
+        document.querySelector(".change-mode-circle i").classList.toggle("fa-moon");
+        document.querySelector(".changeinputdarklight").classList.toggle("input-backgroundchange");
+        document.querySelector(".hero-section #madeby").classList.toggle("madebydark");
+        document.querySelector(".hero-section .block").classList.toggle("block-dark");
+        document.querySelector(".name").classList.toggle("name-dark");
+        document.querySelector(".hero-section").classList.toggle("ondark-hero");
+        document.querySelector(".product-section").classList.toggle("ondark-product")
+    })
+});
+
 
 //                           ANIMAZIONE  "mousemove"
+
+
+
+
+
+esseElement.addEventListener("mousemove",function(dets) {
+    var dims = esseElement.getBoundingClientRect();
+    var xstart = dims.x;
+    var xend = xstart + dims.width;
+
+    var zeroOne = gsap.utils.mapRange(xstart, xend, 0, 1, dets.clientX);
+
+    var dims2 = esseElement.getBoundingClientRect();
+    var ystart = dims2.y;
+    var yend = ystart + dims2.height;
+
+    var zerotwo = gsap.utils.mapRange(ystart, yend, 0, 1, dets.clientY);
+
+    gsap.to(this, {
+        x: lerp(-20, 20, zeroOne),
+        y: lerp(-30, 30, zerotwo)
+    });
+});
+
+esseElement.addEventListener("mouseleave", function() {
+    gsap.to(this, {
+        x: 0,
+        y: 0
+    });
+});
+
 
 
 changemodecircle.addEventListener("mousemove",function(dets){
@@ -269,31 +334,8 @@ changemodecircle.addEventListener("mouseleave",function(){
     })
 })
 
-esseElement.addEventListener("mousemove",function(dets) {
-    var dims = esseElement.getBoundingClientRect();
-    var xstart = dims.x;
-    var xend = xstart + dims.width;
 
-    var zeroOne = gsap.utils.mapRange(xstart, xend, 0, 1, dets.clientX);
 
-    var dims2 = esseElement.getBoundingClientRect();
-    var ystart = dims2.y;
-    var yend = ystart + dims2.height;
-
-    var zerotwo = gsap.utils.mapRange(ystart, yend, 0, 1, dets.clientY);
-
-    gsap.to(this, {
-        x: lerp(-20, 20, zeroOne),
-        y: lerp(-30, 30, zerotwo)
-    });
-});
-
-esseElement.addEventListener("mouseleave", function() {
-    gsap.to(this, {
-        x: 0,
-        y: 0
-    });
-});
 
 
 rainbowHover.addEventListener("mousemove",function(dets) {
@@ -348,6 +390,28 @@ copyright.addEventListener("mouseleave", function() {
     });
 });
 
+function animateScrollElements(selector) {
+    const target = document.querySelectorAll(selector);
+  
+    target.forEach((element) => {
+      var pos = window.pageYOffset * element.dataset.rate;
+  
+      if (element.dataset.direction === 'vertical') {
+        element.style.transform = `translate3d(0px, ${pos}px, 0px)`;
+      } else {
+        var posX = window.pageYOffset * element.dataset.ratex;
+        var posY = window.pageYOffset * element.dataset.ratey;
+  
+        element.style.transform = `translate3d(${posX}px, ${posY}px, 0px)`;
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', function(e) {
+    animateScrollElements('.scroll');
+    animateScrollElements('.immagine');
+    animateScrollElements('.immagine1');
+  });
 
 
 
